4fd46e2176059bddecd9dd65edaa94fa50da44e7
Léon LE GAL <leon@MacBook-Air-de-Leon.local>
2021-11-15 12:29:04 +0100

    seri
    fichier

---
+++ habits/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Habit
+
+
+class HabitSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Habit
+        fields = ('title', 'completed')
