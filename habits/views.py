c4fcf1ac37b479ee6c74cf0741d73fad638b4b26
ChaimaBenmakhlouf <chaima.benmakhlouf@supinternet.fr>
2021-11-15 11:04:41 +0100

    add project habits + add files to git ignore

---
+++ habits/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+
+
+@login_required
+def home(request):
+    current_user = request.user
+
+    habits = Habit.objects.filter(user=current_user.id)
+    form = HabitForm()
+    context = {'habits': habits, 'form': form}
+    return render(request, 'habits/list.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def index(request):
+    current_user = request.user
+
+    habits = Habit.objects.filter(user=current_user.id)
+    form = HabitForm()
+    if request.method == 'POST':
+        form = HabitForm(request.POST)
+        if form.is_valid():
+            localForm = form.save(commit=False)
+            localForm.user = request.user
+            localForm.save()
+        return redirect('/')
+    context = {'habits': habits, 'form': form}
+    return render(request, 'habits/list.html', context)
+
+
+def updateTask(request, pk):
+
+    habit = Habit.objects.get(id=pk)
+    form = HabitForm(instance=habit)
+    if request.method == 'POST':
+        form = HabitForm(request.POST, instance=habit)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'habits/update_task.html', context)
+
+
+def deleteTask(request, pk):
+
+    item = Habit.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item': item}
+    return render(request, 'habits/delete.html', context)
