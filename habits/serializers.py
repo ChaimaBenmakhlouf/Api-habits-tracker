46e2455f72d7029d3e319ec1664a1a6743b397bb
Nasake <wilfrid.salles-otta@supinternet.fr>
2021-11-24 12:14:24 +0100

    error clear

--- habits/serializers.py
+++ habits/serializers.py
@@ -6,7 +6,7 @@ from .models import Habit
 class HabitSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Habit
-        fields = ('title', 'completed')
+        fields = ('title', 'complete')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
